5b0c15f1420eff68e9bd0655912af6e41abccf08
Petter Hancock <pettor@gmail.com>
2023-06-29 14:50:15 +0200

    chore: update MUI theme

--- src/theme/Theme.ts
+++ src/theme/Theme.ts
@@ -8,56 +8,6 @@ import type { PaletteMode } from "@mui/material";
 import type { Theme } from "@mui/material/styles";
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
-declare module "@mui/material/styles" {
-  interface Theme {
-    customBackgrounds: {
-      accent: {
-        light: string;
-        main: string;
-        dark: string;
-      };
-      common: {
-        transparent: string;
-      };
-      gradients: {
-        linear: {
-          variation1: string;
-        };
-      };
-    };
-    borderRadius: {
-      default: string;
-      md: string;
-      lg: string;
-    };
-  }
-  // allow configuration using `createTheme`
-  interface ThemeOptions {
-    customBackgrounds?: {
-      accent: {
-        light: string;
-        main: string;
-        dark: string;
-      };
-      common?: {
-        transparent?: string;
-      };
-      gradients?: {
-        linear?: {
-          variation1?: string;
-        };
-      };
-    };
-    borderRadius?: {
-      default: string;
-      md: string;
-      lg: string;
-    };
-  }
-}
-
-const DEFAULT_BORDER_RADIUS = "4px";
-
 export default function createAppTheme(mode: PaletteMode = "light"): Theme {
   return responsiveFontSizes(
     createTheme({
@@ -96,60 +46,6 @@ export default function createAppTheme(mode: PaletteMode = "light"): Theme {
 
         h1: {
           fontWeight: "500",
-        },
-      },
-      borderRadius: {
-        default: DEFAULT_BORDER_RADIUS,
-        md: "8px",
-        lg: "12px",
-      },
-      customBackgrounds: {
-        accent:
-          mode === "light"
-            ? {
-                light: "#9ea7b1",
-                main: "#aab4be",
-                dark: "#7c838b",
-              }
-            : {
-                light: "#a6b8cb",
-                main: "#8796A5",
-                dark: "#5d6772",
-              },
-        common: {
-          transparent: "transparent",
-        },
-        gradients: {
-          linear: {
-            variation1: "linear-gradient(120deg, #bd34fe 30%, #41d1ff)",
-          },
-        },
-      },
-      components: {
-        MuiButton: {
-          styleOverrides: {
-            root: {
-              minHeight: "42px",
-              borderRadius: "4px",
-              boxShadow: "none",
-
-              "&:hover": {
-                boxShadow: "none",
-              },
-            },
-          },
-        },
-        MuiTextField: {
-          styleOverrides: {
-            root: {
-              borderRadius: DEFAULT_BORDER_RADIUS,
-              transition: "all 5s ease-out",
-
-              "& .MuiOutlinedInput-notchedOutline": {
-                transition: "all 0.1s ease-out",
-              },
-            },
-          },
         },
       },
     })
