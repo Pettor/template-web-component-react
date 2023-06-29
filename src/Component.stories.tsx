09218b83f88a5d48b97bd64f945fa840045de4f8
Petter Hancock <pettor@gmail.com>
2023-06-29 14:49:50 +0200

    feat: add interaction test for component

--- src/Component.stories.tsx
+++ src/Component.stories.tsx
@@ -1,4 +1,6 @@
+import { expect } from "@storybook/jest";
 import type { Meta, StoryObj } from "@storybook/react";
+import { within } from "@storybook/testing-library";
 import { Component } from "./Component";
 import CommonDecorator from "./stories/decorators/CommonDecorator";
 
@@ -15,5 +17,10 @@ type Story = StoryObj<typeof Component>;
 export const WithLabel = {
   args: {
     label: "Hello, World!",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    expect(canvas.getByText("Hello, World!")).toBeInTheDocument();
   },
 } satisfies Story;
