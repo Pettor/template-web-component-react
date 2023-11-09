a63c56b67074ca8131e2d1c0aebf89144941ddcf
Petter Hancock <pettor@gmail.com>
2023-11-09 20:32:34 +0100

    fix: remove storybook play testing

--- src/components/ExampleCard.stories.tsx
+++ src/components/ExampleCard.stories.tsx
@@ -1,6 +1,4 @@
-import { expect } from "@storybook/jest";
 import type { Meta, StoryObj } from "@storybook/react";
-import { within, userEvent } from "@storybook/testing-library";
 import { ExampleCard as Component } from "./ExampleCard";
 import { CommonDecorator } from "~/stories/decorators/CommonDecorator";
 
@@ -17,12 +15,6 @@ type Story = StoryObj<typeof meta>;
 export const WithLabel = {
   args: {
     label: "Hello, World!",
-  },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText("Hello, World!")).toBeInTheDocument();
-
-    await userEvent.click(canvas.getByTestId("example-card__share-button"));
   },
 } satisfies Story;
 
